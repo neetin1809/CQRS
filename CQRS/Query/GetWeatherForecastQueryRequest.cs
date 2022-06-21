5219e2b579cc45907e7081a924686ebee77c391a
neetin1809 <nitin.vaswani@rediffmail.com>
2022-06-21 11:14:49 +0530

    Add project files.

---
+++ CQRS/Query/GetWeatherForecastQueryRequest.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+
+namespace CQRS.CQRS.Query
+{
+    public class GetWeatherForecastQueryRequest: IRequest<IEnumerable<WeatherForecast>>
+    {
+        private static readonly string[] Summaries = new[]
+       {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        public DateTime DateTime { get; set; }
+
+        public class GetWeatherForecastQueryHandler : IRequestHandler<GetWeatherForecastQueryRequest, IEnumerable<WeatherForecast>>
+        {
+            private ILogger<GetWeatherForecastQueryHandler> _logger;
+            public GetWeatherForecastQueryHandler(ILogger<GetWeatherForecastQueryHandler> logger)
+            {
+                _logger = logger;
+            }
+
+            public async Task<IEnumerable<WeatherForecast>> Handle(GetWeatherForecastQueryRequest query, CancellationToken cancellationToken)
+            {
+                var dateTime = query.DateTime;
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = dateTime.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                }).ToArray();
+            }
+        }
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using CQRS.CQRS.Query;
+
+namespace CQRS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+        private readonly IMediator _mediator;
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            return await _mediator.Send(new GetWeatherForecastQueryRequest { DateTime = DateTime.Now });
+        }
+    }
+}
